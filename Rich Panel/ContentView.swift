581dd77a240f8a18b9d9871ba0bd22ba41b9f2eb
PrakharParakh <prakharparakh.10@gmail.com>
2023-08-04 18:43:32 +0530

    Initial Commit

---
+++ Rich Panel/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rich Panel
+//
+//  Created by Prakhar Parakh on 04/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rich Panel/Rich_PanelApp.swift
@@ -0,0 +1,18 @@
+//
+//  Rich_PanelApp.swift
+//  Rich Panel
+//
+//  Created by Prakhar Parakh on 04/08/23.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct Rich_PanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
